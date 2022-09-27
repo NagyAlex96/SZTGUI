8affda5e61c8d0a7477d5a444e8402e0e2278df6
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-09-27 11:58:28 +0200

    .

---
+++ 03_Ora/Model/MainWindowModel.cs
@@ -0,0 +1,52 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _03_Ora.Model
+{
+    internal class MainWindowModel : ObservableObject
+    {
+        private double windowsWidth = SystemParameters.PrimaryScreenWidth / 1.5;
+        public double WindowsWidth
+        {
+            get { return windowsWidth; }
+            set { SetProperty(ref windowsWidth, value); }
+        }
+
+        private double windowsHeight = SystemParameters.PrimaryScreenHeight / 1.5;
+        public double WindowsHeight
+        {
+            get { return windowsHeight; }
+            set { SetProperty(ref windowsHeight, value); }
+        }
+
+        private int strength;
+        public int Strength
+        {
+            get { return strength; }
+            set { SetProperty(ref strength, value); }
+        }        
+        
+        private int vitality;
+        public int Vitality
+        {
+            get { return vitality; }
+            set { SetProperty(ref vitality, value); }
+        }        
+        
+        private int virtue;
+
+        public int Virtue
+        {
+            get { return virtue; }
+            set { SetProperty(ref virtue, value); }
+        }
+
+
+
+    }
+}
---
+++ 03_Ora/ViewModel/MainWindowsVM.cs
@@ -0,0 +1,25 @@
+﻿using _03_Ora.Model;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Ora.ViewModel
+{
+    internal class MainWindowsVM : ObservableObject
+    {
+        public MainWindowModel mainWindowModel  { get; set; }
+
+        public MainWindowsVM()
+        {
+            mainWindowModel = new MainWindowModel();
+
+
+        }
+
+        
+
+    }
+}
