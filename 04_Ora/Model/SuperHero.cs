5409837c01729af3392e3ef386b7f3c1be0eff36
NagyAlex96 <Nagy.Alex96@gmail.com>
2022-09-27 11:59:40 +0200

    .

---
+++ 04_Ora/Model/SuperHero.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Ora.Model
+{
+    internal class SuperHero : ObservableObject
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+
+        private double strength;
+        public double Strength
+        {
+            get { return strength; }
+            set { SetProperty(ref strength, value); }
+        }
+
+        private double pace;
+        public double Pace
+        {
+            get { return pace; }
+            set { SetProperty(ref pace, value); }
+        }
+
+
+
+    }
+}
---
+++ 04_Ora/ViewModel/MainWindowsVM.cs
@@ -0,0 +1,81 @@
+﻿using _04_Ora.Model;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Ora.ViewModel
+{
+    internal class MainWindowsVM : ObservableRecipient
+    {
+        public SuperHeroVM SuperHeroVM { get; private set; }
+        public ObservableCollection<SuperHero> HeroesBarack { get; private set; }
+        public ObservableCollection<SuperHero> HeroesArmy { get; private set; }
+
+        public MainWindowsVM()
+        {
+            this.SuperHeroVM = new SuperHeroVM();
+            this.HeroesBarack = new ObservableCollection<SuperHero>();
+            this.HeroesArmy = new ObservableCollection<SuperHero>();
+
+
+        }
+
+        public IRelayCommand AddToArmyCommand{ get; private set; }
+
+        private void AddToArmy()
+        {
+
+        }        
+        
+        public IRelayCommand AddToBarackCommand{ get; private set; }
+
+        private void AddToBarack()
+        {
+
+        }
+
+        private void SomeBasicHeroes()
+        {
+            this.HeroesArmy.Add(new SuperHero()
+            {
+                Name = "marine",
+                Strength = 5,
+                Pace = 4,
+            });            
+            
+            this.HeroesArmy.Add(new SuperHero()
+            {
+                Name = "marine",
+                Strength = 5,
+                Pace = 4,
+            });            
+            
+            this.HeroesArmy.Add(new SuperHero()
+            {
+                Name = "marine",
+                Strength = 5,
+                Pace = 4,
+            });            
+            
+            this.HeroesArmy.Add(new SuperHero()
+            {
+                Name = "marine",
+                Strength = 5,
+                Pace = 4,
+            });           
+            
+            this.HeroesArmy.Add(new SuperHero()
+            {
+                Name = "marine",
+                Strength = 5,
+                Pace = 4,
+            });
+        }
+
+    }
+}
